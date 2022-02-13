ecd180047ab1dcab124048fa406daceb502e5d21
Mohammed Hesham Farhan <58566929+Mohamad-Farhan@users.noreply.github.com>
2022-02-13 22:02:16 +0200

    add the server and installing the needed packages

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+// app
+const app = express();
+
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(cors());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`server is runing on ${PORT}`));

17dc4cf0934050cbe93bf2659f0f74197f2c11fe
Mohammed Hesham Farhan <58566929+Mohamad-Farhan@users.noreply.github.com>
2022-02-13 22:38:10 +0200

    Create Product schema

---
+++ server/database/models/product.js
@@ -0,0 +1,83 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 100,
+      text: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      text: true,
+    },
+    others: {
+      type: String,
+      required: true,
+      text: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
+      maxlength: 50,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+    },
+    subs: [
+      {
+        type: ObjectId,
+        ref: "Sub",
+      },
+    ],
+    quantity: Number,
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    images: {
+      type: Array,
+    },
+    shipping: {
+      type: Number,
+      required: true,
+      trim: true,
+      maxlength: 4,
+    },
+    colors: [
+      {
+        type: ObjectId,
+        ref: "Color",
+      },
+    ],
+    brands: [
+      {
+        type: ObjectId,
+        ref: "Brand",
+      },
+    ],
+    ratings: [
+      {
+        star: Number,
+        postedBy: { type: ObjectId, ref: "User" },
+        comment: { type: String, required: true },
+        name: { type: String, require: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
